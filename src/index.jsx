d60e9dbdf8e6664e540469e20eb9c539631640fa
John Aspinall <john.aspinall73@gmail.com>
2022-06-01 14:38:17 +0100

    Just the CSS background left to do

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,28 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import './css/index.scss'; // Tell webpack about index.scss
-import logo from './images/logo192.png'; // Telling webpack that index.js uses this image file
+import Logo from './images/logo192.png'; 
+import Background from './images/logo512.png'
 
 ReactDOM.render( 
-   <div className="wrapper">
+   <div className="wrapper" style={{ backgroundImage:`url(${Background})`,backgroundRepeat:"no-repeat",  }}>
       <header>
          <div className="logo-header">
-            <img src={logo} alt="React logo" className="logo-img"  />
+            <img src={Logo} alt="React logo" className="logo-img"  />
             <h1 className="logo-h1">ReactFacts</h1>
          </div>
          <nav className="header-nav">React Course - Project 1</nav>
       </header>
-      <div className="content">
+      <main className="content" >
          <h2>Fun facts about react</h2>
-         <ol>
+         <ul>
             <li>First released in 2013</li>
             <li>Created by Jordan Walke</li>
             <li>Has well over 100K stars on Github</li>
             <li>Is maintained by Facebook</li>
             <li>Powers loads of enterise apps including mobile apps</li>
-         </ol>  
-      </div>
+         </ul>  
+      </main>
    </div>
    ,
    document.getElementById('root')
